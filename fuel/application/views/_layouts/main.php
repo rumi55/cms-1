52352b09481fd93857e1d42123c17417f3642b34
bhagdave <dave_gill@blueyonder.co.uk>
2014-01-08 16:31:27 +0000

    Changed the side bar to include stoneacre

--- fuel/application/views/_layouts/main.php
+++ fuel/application/views/_layouts/main.php
@@ -4,11 +4,11 @@
         <div class="sidebar">
           <h3>Links to my commercial web development work</h3>
           <ul>
-            <li><a href="http://www.cabfix.com">Cabfix.com personal website</a></li>
+			<li><a href="http://www.stoneacre.co.uk">Stoneacre Website</a></li>
+          	<li><a href="http://www.cabfix.com">Cabfix.com personal website</a></li>
             <li><a href="http://www.cabfix.biz">Cabfix Business site</a></li>
-            <li><a href="http://Cabfix.mobi">Cabfix - Mobile web app for booking taxis</a></li>
             <li><a href="http://jow.st">Jowst - Games tournaments</a></li>
-	    <li><a href="http://www.gamesgrabr.com">GRABR - Social network for gamers</a></li>
+	    	<li><a href="http://www.gamesgrabr.com">GRABR - Social network for gamers</a></li>
           </ul>
         </div>
 </div>
